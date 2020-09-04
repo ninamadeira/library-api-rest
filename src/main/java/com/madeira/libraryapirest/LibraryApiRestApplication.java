e6308eceb82a215ffe18f3dbfeff7da4e16c87ac
Nina Rosa Madeira <nina.madeira@gmail.com>
2020-09-04 02:53:51 -0300

    commit

--- src/main/java/com/madeira/libraryapirest/LibraryApiRestApplication.java
+++ src/main/java/com/madeira/libraryapirest/LibraryApiRestApplication.java
@@ -1,9 +1,10 @@
 package com.madeira.libraryapirest;
 
-import org.modelmapper.ModelMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
 public class LibraryApiRestApplication {
